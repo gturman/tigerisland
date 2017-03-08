40938f094a620d6589feb0a9feefb2b9aadf5ed1
Geoff <gturman@gmail.com>
2017-03-08 16:12:53 -0500

    committing some actual code here -G

---
+++ src/Hex.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Geoff on 3/8/17.
+ */
+public class Hex {
+    //this is... the BEGINNING!
+}
---
+++ test/GameBoardTest.java
@@ -0,0 +1,6 @@
+/**
+ * Created by geoff on 3/8/17.
+ */
+public class GameBoardTest {
+    //some testing stuff should happen in here, for sure
+}
