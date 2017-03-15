6578c1ec6aadec9cc5d4d6ac7345382e28fec800
BRENDANS-PC\Brendan <bpcameron@ufl.edu>
2017-03-15 12:39:08 -0400

    Forcing me to commit this unstaged to pull

--- src/Player.java
+++ src/Player.java
@@ -1,7 +1,69 @@
 /**
  * Created by William on 3/14/2017.
+ * Edited by BC and CC
  */
 public class Player {
 
+    private int meepleCount;
+    private int totoroCount;
+    private int playerID;
+    private int score;
+    private int settlementCount;
+
+
+    Player(){
+        //todo: assign playerID or initialize as Player P1 and Player P2
+        int score = 0;
+        int meepleCount = 20;
+        int totoroCount = 3;
+        int settlementCount = 0;
+
+        this.score = score;
+        this.meepleCount = meepleCount;
+        this.totoroCount = totoroCount;
+        this.settlementCount = settlementCount;
+
+    }
+
+    public int getMeepleCount(){
+        return meepleCount;
+    }
+
+    public void modifyMeepleCount(int modifier){
+        meepleCount -= modifier;
+    }
+
+    public int getTotoroCount(){
+        return totoroCount;
+    }
+
+    public void decreaseTotoroCount(){
+        this.totoroCount--;
+    }
+
+    public int getSettlementCount(){
+        return settlementCount;
+    }
+
+    public void setSettlementCount(int settlementCount){
+        this.settlementCount = settlementCount;
+    }
+
+    public int getScore(){
+        return score;
+    }
+
+    public void increaseScore(int points){
+        this.score+=points;
+    }
+    /*
+    public int getPlayerID(){
+        return playerID;
+    }
+
+    public void setPlayerID(int playerID){
+        this.playerID = playerID;
+    }
+    */
 }
 
---
+++ test/PlayerTest.java
@@ -0,0 +1,74 @@
+/**
+ * Created by Brendan on 3/15/2017.
+ */
+import org.junit.Assert;
+//import org.junit.Before;
+//import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class PlayerTest {
+
+    @Test
+    public void testPlayerCreation(){
+        Player P1 = new Player();
+        Assert.assertTrue(P1 instanceof Player);
+
+    }
+
+    @Test
+    public void testGetMeepleCount(){
+        Player P1 = new Player();
+        Assert.assertEquals(P1.getMeepleCount(),20);
+
+    }
+
+    @Test
+    public void testGetTotoroCount(){
+        Player P1 = new Player();
+        Assert.assertEquals(P1.getTotoroCount(),3);
+
+    }
+
+    @Test
+    public void testSettlementCount(){
+        Player P1 = new Player();
+        Assert.assertEquals(P1.getSettlementCount(),0);
+    }
+
+    @Test
+    public void testInitialScore(){
+        Player P1 = new Player();
+        Assert.assertEquals(P1.getScore(), 0 );
+    }
+
+    @Test
+    public void testModifiedScore(){
+        Player P1 = new Player();
+        P1.increaseScore(30);
+        Assert.assertEquals(P1.getScore(),30);
+    }
+
+    @Test
+    public void testModifiedMeepleCount(){
+        Player P1 = new Player();
+        Assert.assertEquals(P1.getMeepleCount(),20);
+        P1.modifyMeepleCount(5);
+        Assert.assertEquals(P1.getMeepleCount(),15);
+    }
+
+    @Test
+    public void testModifiedTotoroCount(){
+        Player P1 = new Player();
+        Assert.assertEquals(P1.getTotoroCount(), 3);
+        P1.decreaseTotoroCount();
+        Assert.assertEquals(P1.getTotoroCount(),2);
+    }
+
+    @Test
+    public void testSetSettlementCount(){
+        Player P1 = new Player();
+        P1.setSettlementCount(3);
+        Assert.assertEquals(P1.getSettlementCount(),3);
+    }
+
+}
